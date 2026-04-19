18bdc01ea9cc36c00cc913a7d688e5026002e9aa
Andres Crucitti <dasc495@gmail.com>
2026-04-19 14:22:12 -0700

    fu vite

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,36 @@
 import {
-    defineConfig
+    defineConfig, loadEnv
 } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        host: '0.0.0.0', // allows access from other devices
-        cors: true,
-        hmr: {
-            host: 'vite.insights.dev.local.test', // your laptop's IP145
-            clientPort: 80
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            tailwindcss(),
+        ],
+        server: {
+            host: '0.0.0.0',
+            cors: true,
+            hmr: {
+                host: env.VITE_HMR_HOST || 'localhost',
+                clientPort: env.VITE_HMR_PORT
+                    ? Number(env.VITE_HMR_PORT)
+                    : undefined,
+            },
+            watch: {
+                ignored: [
+                    '**/storage/**',
+                    '**/vendor/**',
+                    '**/node_modules/**'
+                ],
+            },
         },
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
+    }
 });
